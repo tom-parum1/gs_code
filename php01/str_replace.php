12d6bc145006afe4126d2844db3ec119475be0f5
takemura tomoka <smile_flute_0119@yahoo.co.jp>
2020-05-29 16:42:31 +0900

    課題２

---
+++ php01/str_replace.php
@@ -0,0 +1,17 @@
+<?php
+
+$a = "PHP5_PHP6_PHP7_PHP8";
+
+$b = str_replace("PHP","JS",$a);
+echo $b;
+
+?>
+
+//答え
+//<?php
+//$a = "PHP5_PHP6_PHP7_PHP8";
+
+// $b = str_replace("PHP", "JS", $a);
+// echo $b;
+
+// ?>
